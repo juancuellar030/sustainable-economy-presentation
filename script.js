8b50ae624d4d0a38c21e0363af6d45b09cbf5510
juancuellar030 <juancuellar030@gmail.com>
2025-08-03 12:29:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const layers = document.querySelectorAll('.bg-layer');
-    let currentLayer = 0;
 
-    function changeLayer() {
-        layers[currentLayer].style.opacity = 0;
-        currentLayer = (currentLayer + 1) % layers.length;
-        layers[currentLayer].style.opacity = 1;
-    }
-
-    setInterval(changeLayer, 5000);
-});
